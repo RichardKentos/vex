c14e393790df2ea58c77469acd9e610de7524c3b
RichardKentos <77812166+RichardKentos@users.noreply.github.com>
2021-02-21 16:44:31 +0100

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,39 @@
+const menuIcon = document.querySelector(".burger");
+const arrow = document.querySelector(".arrow");
+const transformMenu = document.querySelector(".transform_menu");
+
+menuIcon.addEventListener("click", function() {
+	transformMenu.classList.add("menu_shown");
+})
+
+arrow.addEventListener("click", function() {
+	transformMenu.classList.remove("menu_shown");
+})
+
+
+// ---------------------------
+// BACK TO THE TOP
+// ---------------------------
+
+var back_top = document.getElementById("back_top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = scrollFunction();
+
+function scrollFunction() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    back_top.style.display = "block";
+
+  } else {
+    back_top.style.display = "none";
+  }
+}
+
+
+const backTop = document.querySelector("#back_top");
+
+backTop.click(function() {
+	document.body.animate({
+		scrollTop:0}, "fast");
+});
+
